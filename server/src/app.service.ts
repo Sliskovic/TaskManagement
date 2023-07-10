4532c685b64505c775b5561fcab2be7f82254ed9
Mara <85956880+Sliskovic@users.noreply.github.com>
2023-07-10 09:07:40 +0200

    Update app.service.ts

--- server/src/app.service.ts
+++ server/src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello, Welcome!';
+    return 'Hello, Welcome to Task Management App!';
   }
 }
